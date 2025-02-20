1baf6874d575d84f0daaddb5eaf2df917d04e95c
vijaykumar-06 <58992507+vijaykumar-06@users.noreply.github.com>
2025-02-20 01:13:40 +0000

    Add project files.

---
+++ PaymentApi/Models/PaymentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(116)")]
+        public string CardNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpiryDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+    }
+}
---
+++ PaymentApi/Models/PaymentDetailsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+    }
+}
